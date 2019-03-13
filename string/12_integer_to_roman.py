2d2bd72d74a213e2c28456e56be2a4c5e44442ea
fuping <fuping1@staff.weibo.com>
2019-03-13 19:32:38 +0800

    up

--- string/12_integer_to_roman.py
+++ string/12_integer_to_roman.py
@@ -20,38 +20,19 @@ ROMAN = {'M': 1000,
          'V': 5, 
          'IV': 4, 
          'I': 1}
+ROMAN_LIST = sorted(ROMAN.items(),key=lambda x:x[1], reverse=True)
 class Solution:
     def intToRoman(self, num: int) -> str:
         if num > 3999:
             return ''
         res = []
-        for r, d in ROMAN.items():
+        for r, d in ROMAN_LIST:
             while num >= d:
+                print(num, d)
                 res.append(r)
                 num -= d
         return ''.join(res)
 
-    def get_digit_1(self, num: int) -> list:
-        res = []
-        while num:
-            num , r = divmod(num, 10)
-            res.append(r)
-        res.reverse()
-        return res
-
-    def get_digit_2(self, num: int) -> list:
-        res = []
-        while num:
-            res.append(num % 10)
-            num = num // 10 # floor division
-        res.reverse()
-        return res
-
-    def get_digit_3(self, num: int) -> list:
-        return list(map(int, str(num)))
-
-
 s = Solution()
-print(s.get_digit_1(58))
-print(s.get_digit_2(58))
-print(s.get_digit_3(58))
+print(s.intToRoman(58))
+print(s.intToRoman(1987))
