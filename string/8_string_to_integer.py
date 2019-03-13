7d94820e66dfec131f791d2b5a10f7cb78009a31
fuping <fuping1@staff.weibo.com>
2019-03-13 15:16:41 +0800

    update 8.

--- string/8_string_to_integer.py
+++ string/8_string_to_integer.py
@@ -1,4 +1,28 @@
-import os
-Class Str2Int(object):
+import os, re, sys
+INT_MAX = 2**31-1
+INT_MIN = -2**31
+print(INT_MAX)
+print(INT_MIN)
+class Str2Int(object):
     def atoi(self, str: str) -> int:
-        pass
+        pattern = re.compile(r'^(-|\+| +|)+[0-9]+')
+        # num =  pattern.search(str)
+        num = re.match(r'^(-|\+)?[0-9]+', str.strip())
+        if num:
+            num = int(num.group().strip('+'))
+            if num < INT_MIN:
+                return INT_MIN
+            elif num > INT_MAX:
+                return INT_MAX
+            else:
+                return num
+        else:
+            return 0
+
+x = Str2Int()
+print(x.atoi('  -89'))
+print(x.atoi('4193 with words'))
+print(x.atoi('-91283472332'))
+print(x.atoi('42'))
+print(x.atoi('words and 987'))
+print(x.atoi('+-2'))
